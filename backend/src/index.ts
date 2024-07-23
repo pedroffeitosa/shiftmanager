e10a8cc1d2bba40658fd4b1d5f487c22f1f13892
Pedro F. <jppfeitosa@gmail.com>
2024-07-23 05:17:28 -0300

    fix: Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,27 +3,17 @@ import cors from 'cors';
 import doctorRoutes from './routes/doctorRoutes';
 import hospitalRoutes from './routes/hospitalRoutes';
 import shiftRoutes from './routes/shiftRoutes';
-import createConnection from './database';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Configurar CORS
-const allowedOrigins = ['https://shiftmanager-jade.vercel.app'];
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
+const corsOptions = {
+  origin: ['https://shiftmanager-jade.vercel.app'], // Adicione aqui os domínios permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
 };
-app.use(cors(options));
 
-// Middleware para logs
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/doctors', doctorRoutes);
@@ -34,12 +24,6 @@ app.get('/test', (req, res) => {
   res.send('API is working!');
 });
 
-// Iniciar o servidor
-app.listen(port, async () => {
-  try {
-    const connection = await createConnection();
-    console.log(`Server is running on port ${port}`);
-  } catch (error) {
-    console.error('Não foi possível conectar ao banco de dados. O servidor não será iniciado.');
-  }
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
